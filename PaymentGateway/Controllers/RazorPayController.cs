d30ce1c183f9455b050aac8857988bc7dc21c998
bunty <buntysonimail@gmail.com>
2024-11-13 17:13:12 +0530

    commit

--- PaymentGateway/Controllers/RazorPayController.cs
+++ PaymentGateway/Controllers/RazorPayController.cs
@@ -14,17 +14,12 @@ namespace PaymentGateway.Controllers
         }
         public IActionResult Index()
         {
-            // Configure Razorpay client
             RazorpayClient client = new RazorpayClient(key, secret);
-
-            // Order details
             Dictionary<string, object> options = new Dictionary<string, object>();
             options.Add("amount", 50000);
             options.Add("currency", "INR");
             options.Add("receipt", "order_rcptid_11");
             options.Add("payment_capture", 1);
-
-            // Create order
             Razorpay.Api.Order order = client.Order.Create(options);
             ViewBag.OrderId = order["id"].ToString();
             ViewBag.RazorpayKey = key;
@@ -33,19 +28,14 @@ namespace PaymentGateway.Controllers
 
         public ActionResult PaymentSuccess(string razorpay_payment_id, string razorpay_order_id, string razorpay_signature)
         {
-            // Validate the payment signature
             Dictionary<string, string> attributes = new Dictionary<string, string>();
             attributes.Add("razorpay_payment_id", razorpay_payment_id);
             attributes.Add("razorpay_order_id", razorpay_order_id);
             attributes.Add("razorpay_signature", razorpay_signature);
-
             Utils.verifyPaymentSignature(attributes);
-
-            // Handle success logic here, such as updating the order status
             ViewBag.PaymentId = razorpay_payment_id;
             ViewBag.OrderId = razorpay_order_id;
-
-            return View("Success");
+            return RedirectToAction("Success","Home");
         }
     }
 }
