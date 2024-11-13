1a9033894826371ba827ba6265c3924e351d5773
bunty <buntysonimail@gmail.com>
2024-11-13 18:22:41 +0530

    razorpay payment gateway integration added

--- PaymentGateway/Controllers/RazorPayController.cs
+++ PaymentGateway/Controllers/RazorPayController.cs
@@ -6,23 +6,30 @@ namespace PaymentGateway.Controllers
     public class RazorPayController : Controller
     {
         private readonly IConfiguration _configuration;
-        private string key = "xxx";
-        private string secret = "xxx";
+        private string key = "***";
+        private string secret = "***";
         public RazorPayController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
         public IActionResult Index()
         {
+            ViewBag.RazorpayKey = key;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PaymentGateway.Models.Payment payment)
+        {
             RazorpayClient client = new RazorpayClient(key, secret);
             Dictionary<string, object> options = new Dictionary<string, object>();
-            options.Add("amount", 50000);
+            options.Add("amount", payment.Amount * 100);
             options.Add("currency", "INR");
             options.Add("receipt", "order_rcptid_11");
             options.Add("payment_capture", 1);
             Razorpay.Api.Order order = client.Order.Create(options);
             ViewBag.OrderId = order["id"].ToString();
-            ViewBag.RazorpayKey = key;
+            
             return View();
         }
 
--- PaymentGateway/Models/Payment.cs
+++ PaymentGateway/Models/Payment.cs
@@ -2,6 +2,8 @@
 {
     public class Payment
     {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
         public decimal Amount { get; set; }
         public string? Mobile { get; set; }
     }
